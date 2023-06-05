03c14b0d87775b0711f45f770510b4abc345ff3a
Josh Gulledge <jgulledge19@gmail.com>
2023-06-05 17:33:21 -0600

    Set up routers

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for sheetmetalbackend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from jobdata.views import *
+
+# Create a Router
+router = routers.DefaultRouter()
+# Register viewset with the router
+router.register(r'job', JobViewSet)
+router.register(r'download', DownloadViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path("admin/", admin.site.urls),
 ]
