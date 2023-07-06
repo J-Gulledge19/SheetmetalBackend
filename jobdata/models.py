ca71f1bdc139c4d6838bb0f1bb897f2a6241a3ae
Josh Gulledge <jgulledge19@gmail.com>
2023-07-06 17:59:41 -0600

    Removed weights model and added values to download model

--- jobdata/models.py
+++ jobdata/models.py
@@ -11,22 +11,27 @@ class Job(models.Model):
 class Download(models.Model):
     dlName = models.CharField(max_length=50)
     done = models.BooleanField()
-    dateTurnedIn = models.CharField(max_length=10)
-    dateDue = models.CharField(max_length=10)
+    dateTurnedIn = models.CharField(max_length=40)
+    dateDue = models.CharField(max_length=40)
+    receivedBy = models.CharField(max_length=20)
+    fittings = models.IntegerField()
+    cutOut = models.BooleanField()
+    duct = models.IntegerField()
     active = models.BooleanField()
+    weight = models.IntegerField()
     job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='downloads')
     
     def __str__(self):
         return self.dlName
     
-class Weight(models.Model):
-    galv = models.IntegerField()
-    ss = models.IntegerField()
-    blackIron = models.IntegerField()
-    pl = models.IntegerField()
-    alum = models.IntegerField()
-    dl = models.OneToOneField(Download, on_delete=models.CASCADE, related_name='weight', null=True, blank=True)
+# class Weight(models.Model):
+#     galv = models.IntegerField()
+#     ss = models.IntegerField()
+#     blackIron = models.IntegerField()
+#     pl = models.IntegerField()
+#     alum = models.IntegerField()
+#     dl = models.OneToOneField(Download, on_delete=models.CASCADE, related_name='weight', null=True, blank=True)
     
-    def __str__(self):
-        return self.galv
+#     def __str__(self):
+#         return self.galv
     
