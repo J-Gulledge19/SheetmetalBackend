22b7967decfcf7f0196f04a9ca6c21ba29126cd7
Josh Gulledge <jgulledge19@gmail.com>
2023-07-06 17:58:47 -0600

    Commented out unused code

--- jobdata/serializers.py
+++ jobdata/serializers.py
@@ -1,23 +1,23 @@
-from .models import Job, Download, Weight
+from .models import Job, Download
 from rest_framework import serializers
 
-class WeightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Weight
-        fields = '__all__'
+# class WeightSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Weight
+#         fields = '__all__'
         
 class DownloadSerializer(serializers.ModelSerializer):
-    weight = WeightSerializer(many = False)
+    # weight = WeightSerializer(many = False)
     class Meta:
         model = Download
         fields = '__all__'
-    def create(self, validated_data):
-        print(validated_data)
-        weight_data = validated_data.pop('weight')
-        print(weight_data)
-        download = Download.objects.create(**validated_data)
-        Weight.objects.create(dl=download, **weight_data)
-        return download
+    # def create(self, validated_data):
+    #     print(validated_data)
+    #     weight_data = validated_data.pop('weight')
+    #     print(weight_data)
+    #     download = Download.objects.create(**validated_data)
+    #     Weight.objects.create(dl=download, **weight_data)
+    #     return download
         
 class JobSerializer(serializers.ModelSerializer):
     downloads = DownloadSerializer(many = True, read_only=True)
