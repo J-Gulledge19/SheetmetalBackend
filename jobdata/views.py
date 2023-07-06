7eeddc32e466a8f9f0f409f8edb9aa38c77a86f3
Josh Gulledge <jgulledge19@gmail.com>
2023-07-06 17:58:03 -0600

    Removed unused import of weights

--- jobdata/views.py
+++ jobdata/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Job, Download, Weight
+from .models import Job, Download
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import JobSerializer, DownloadSerializer, WeightSerializer
+from .serializers import JobSerializer, DownloadSerializer
 
 # Create your views here.
 
